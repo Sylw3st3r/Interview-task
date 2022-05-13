091147e97606b9bd684925076c1eeee087138e37
Sylwester Kucia <sylwester.j.kucia@gmail.com>
2022-05-13 08:41:13 +0200

    added function that finds furthest users

--- app.js
+++ app.js
@@ -3,36 +3,35 @@ const cartsUrl = "https://fakestoreapi.com/carts";
 const productsUrl = "https://fakestoreapi.com/products"
 
 //1. Retrieves user, product and shopping cart data
-function getAllData() {
+const getAllData = async () => {
     const requests = [getData(usersUrl),getData(cartsUrl),getData(productsUrl)];
+    const data = await Promise.all(requests)
+    return data;
+ } 
 
-    Promise.all(requests)
-        .then((responses) => Promise.all(responses.map(response => response.json())))
-        .then(([users,carts,products]) => doSmthing(users,carts,products))
-        .catch(err => {
-            console.log(`Something went wrong. ${err.message}`)
-        })
-    }
-    
-async function getData(url) {
-    return fetch(url, {
+ const getData = async url => {
+    const response = await fetch(url, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         },
     });
+    const data = await response.json();
+    return data
 }
 
-function doSmthing(users,carts,products){
+const doSmthing = async () =>{
+    const [users, carts, products] = await getAllData();
     console.log(users);
     console.log(carts);
     console.log(products);
     findUserWithTheMostExpensiveCart(users,carts,products);
     splitProductsIntoCategories(products);
     console.log(countTotalPriceOfEachCategory(products));
+    console.log(findFurthestUsers(users));
 }
 
-function splitProductsIntoCategories(products){
+const splitProductsIntoCategories = products =>{
     const categories = new Set()
     products.forEach(product => categories.add(product.category));
     const categoryDataSets = [...categories].map(category => {
@@ -42,7 +41,7 @@ function splitProductsIntoCategories(products){
 }
 
 //Creates a data structure containing all available product categories and the total value of products of a given category
-function countTotalPriceOfEachCategory(products){
+const countTotalPriceOfEachCategory = products =>{
     const categories = splitProductsIntoCategories(products);
     products.forEach(product => {
         categories.forEach(category => {
@@ -57,23 +56,23 @@ function countTotalPriceOfEachCategory(products){
     return categories;
 }
 
-function getPrice(id, products){
+const getPrice = (id, products) => {
     return products.find(product => product.id === id).price;
 }
 
-function getTotalPriceOfCart(cart,products){
+const getTotalPriceOfCart = (cart,products) =>{
     return cart.products.reduce((previousValue, currentValue) => {
         return previousValue + getPrice(currentValue.productId, products)*(currentValue.quantity);
     }, 0);
 }
  
-function getFullNameOfUser(id, users){
+const getFullNameOfUser = (id, users) => {
     const user = users.find(user => user.id === id);
     return user.name;
 }
 
 //Finds a cart with the highest value
-function getTheMostExpensiveCart(carts, products){
+const getTheMostExpensiveCart = (carts, products) => {
     //Get total price for each cart
     const data = carts.map(cart => {
         return {...cart, totalPrice:getTotalPriceOfCart(cart, products)}
@@ -87,7 +86,7 @@ function getTheMostExpensiveCart(carts, products){
 }
 
 //3. Finds a cart with the highest value, determines its value and full name of its owner
-function findUserWithTheMostExpensiveCart(users,carts,products){
+const findUserWithTheMostExpensiveCart = (users,carts,products) => {
     //Finds a cart with the highest value
     const cart = getTheMostExpensiveCart(carts,products);
     //Name of its owner
@@ -95,4 +94,59 @@ function findUserWithTheMostExpensiveCart(users,carts,products){
     console.log(`The Most Expensive Cart's id: ${cart.id}, Total price: ${cart.totalPrice}, User: ${user.firstname} ${user.lastname}`)
 }
 
-getAllData();
+//Haversine formula
+const getDistance = (A, B) => {
+    
+    const {lat:lat1,long:lon1} = A;
+    const {lat:lat2,long:lon2} = B;
+
+    const radius = 6371; 
+    const dLat = deg2rad(lat2-lat1);
+    const dLon = deg2rad(lon2-lon1); 
+    const a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2); 
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    const d = radius * c;
+    return d;
+}
+  
+const deg2rad = deg => {
+    return deg * (Math.PI/180) 
+}
+
+const getAllUserDistanceCombinations = (users) => {
+
+    console.log(users);
+    const allCombinations = [];
+
+    for(let i = 0; i<users.length;i++){
+        for(let j = i; j<users.length;j++){
+            const user1 = users[i];
+            const user2 = users[j];
+            const A = user1.address.geolocation;
+            const B = user2.address.geolocation;
+
+            const combination = {
+                user1:user1,
+                user2:user2,
+                distance:getDistance(A, B)
+            }
+            console.log(combination);
+            allCombinations.push(combination);
+        }
+    }
+    return allCombinations;
+}
+
+//4. Finds the two users living furthest away from each other
+const findFurthestUsers = users => {
+    const allCombinations = getAllUserDistanceCombinations(users);
+    return closestPar = allCombinations.reduce((previousValue, currentValue) => {
+        return currentValue.distance > previousValue.distance ? currentValue : previousValue;
+    })
+}
+
+
+doSmthing();
